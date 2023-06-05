76f4c8c1532810d5559b3bb9478e213e043fcbf6
Thiagosdep <thiago_sousap@hotmail.com>
2023-06-05 17:45:37 -0300

    move AxiosHttpClient to its own file

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -1,8 +1,9 @@
-import { HttpGetClient } from '@/infra/http'
+import { AxiosHttpClient } from '@/infra/http'
 
 import axios from 'axios'
 
 jest.mock('axios')
+
 describe('AxiosHttpClient', () => {
   let sut: AxiosHttpClient
   let fakeAxios: jest.Mocked<typeof axios>
