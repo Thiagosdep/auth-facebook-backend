af74c7b94c09cf66d51881a7fa9896e7dffe2ffd
Thiagosdep <thiago_sousap@hotmail.com>
2023-06-06 02:21:46 -0300

    ensure PgUserAccountRepository updates an account if id is defined

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -53,5 +53,28 @@ describe('PostgresUserAccountRepository', () => {
 
       expect(pgUser?.id).toBe(1)
     })
+
+    it('should update an account if id is defined', async () => {
+      await pgUserRepo.save({
+        email: 'any_email',
+        name: 'any_name',
+        facebookId: 'any_facebookId'
+      })
+
+      await sut.saveWithFacebook({
+        id: '1',
+        email: 'new_email',
+        name: 'new_name',
+        facebookId: 'new_facebookId'
+      })
+      const pgUser = await pgUserRepo.findOne({ id: 1 })
+
+      expect(pgUser).toEqual({
+        id: 1,
+        email: 'any_email',
+        name: 'new_name',
+        facebookId: 'new_facebookId'
+      })
+    })
   })
 })
